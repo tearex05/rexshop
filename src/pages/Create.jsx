fc65e4460630af96f3626194616a51966f59deff
tearex05 <tearex05@yahoo.com>
2023-03-29 14:20:58 +0430

    last

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import FileBase64 from "react-file-base64";
 
 function Create() {
 	const navigate = useNavigate();
@@ -15,6 +16,7 @@ function Create() {
 		error: "",
 		btn: "Submit",
 	});
+	const [path, setPath] = useState("");
 	const changeHandler = (e) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
@@ -28,21 +30,16 @@ function Create() {
 			!formData.image
 		) {
 			setFormData({ ...formData, error: "Plz fill every field" });
-		} else if(formData.image.size >= 3000000){
-			setFormData({...formData, error: "Image size should be < 3mb"})
+		} else if (formData.image.size >= 3000000) {
+			setFormData({ ...formData, error: "Image size should be < 3mb" });
 		} else {
-			setFormData({...formData, btn: "Plz Wait..."})
-			let data = new FormData();
-			data.append("name", formData.name);
-			data.append("price", formData.price);
-			data.append("category", formData.category);
-			data.append("creator", formData.creator);
-			data.append("info", formData.info);
-			data.append("image", formData.image);
-			axios.post("https://rexshop.onrender.com/createitem", data).then((res) => {
-				setFormData({ ...formData, btn: "Submit" });
-				navigate("/rexshop");
-			});
+			setFormData({ ...formData, btn: "Plz Wait..." });
+			axios
+				.post("https://rexshop.onrender.com/createitem", formData)
+				.then((res) => {
+					setFormData({ ...formData, btn: "Submit" });
+					navigate("/rexshop");
+				});
 		}
 	};
 	if (!user) {
@@ -70,6 +67,13 @@ function Create() {
 	return (
 		<div className="flex flex-col w-full h-full items-center justify-center pt-20 pb-20">
 			<h1 className="text-4xl font-bold">Add An Item</h1>
+			<button
+				onClick={() => {
+					console.log(formData);
+				}}
+			>
+				Test
+			</button>
 			<form
 				className="flex flex-col items-center mt-6 shadow-2xl rounded-2xl p-5 w-2/3 max-w-2xl"
 				onSubmit={submitHandler}
@@ -118,16 +122,16 @@ function Create() {
 					<option value="house">House</option>
 				</select>
 				<label htmlFor="picture">Picture:</label>
-				<input
-					id="picture"
-					type="file"
-					name="picture"
-					onChange={(e) =>
-						setFormData({ ...formData, image: e.target.files[0] })
-					}
-					accept=".png"
+				<span
 					className="border-2 border-black w-10/12 max-w-4xl p-2 mb-4"
-				/>
+					id="picture"
+					name="picture"
+				>
+					<FileBase64
+						multiple={false}
+						onDone={({base64}) => setFormData({...formData, image: base64})}
+					/>
+				</span>
 				<p className="text-red-500">{formData.error}</p>
 				<input
 					type="submit"
